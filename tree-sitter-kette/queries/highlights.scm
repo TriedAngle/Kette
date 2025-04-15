819c4475a6562ef0ebdffefc25dba46138c65e54
Sebastian Strobl <Sebastian@Strobl.net>
2025-04-15 21:34:48 +0200

    Add: more highlights

--- tree-sitter-kette/queries/highlights.scm
+++ tree-sitter-kette/queries/highlights.scm
@@ -1,12 +1,12 @@
 (identifier) @Identifier
 
 ((identifier) @Keyword
- (#any-of? @Keyword if self when unless 4drop dip keep 2keep 3keep 4keep 
-    bi 2bi "bi*" "2bi*" "bi@" "2bi@" get set unbox slot set-slot ">box"
+ (#any-of? @Keyword if self when unless 4drop dip 2dip 3dip 4dip keep 2keep 3keep 4keep 
+    bi 2bi "bi*" "2bi*" "bi@" "2bi@" tri "tri*" "tri@" get set unbox slot set-slot set-u8 get-u8 ">box"
     "(call)" call ">r" "r>" "r@"))
 
 ((identifier) @Function
-  (#any-of? @Function "+" "-" "*" "/" "not" "neg" ))
+  (#any-of? @Function "+" "-" "*" "/" "not" "neg" "map" "ref-eq?"))
 
 ((identifier) @Repeat
  (#match? @Repeat "loop|while"))
@@ -19,10 +19,10 @@
   (#any-of? @Operator dup 2dup 3dup 4dup dupd drop 2drop 3drop dropd swap swapd 2swap over overd rot -rot pick rotd -rotd roll -roll))
 
 ((identifier) @Punctuation.Special.Query
-  (#any-of? @Punctuation.Special.Query "(new-boa)" "(new)" "(special)" "(clone)" "<array>" "<bytearray>" 1array 2array 3array 4array 5array))
+  (#any-of? @Punctuation.Special.Query "(new-boa)" "(new)" "(special)" "(clone)" "<array>" "<bytearray>" "resize-array" "resize-bytearray" "copy-array" "copy-bytearray" 1array 2array 3array 4array 5array))
 
 ((identifier) @Function.Builtin
-  (#any-of? @Function.Builtin "\\" "@read-next" "@parse-int" "@parse-int" "@read-until" "@skip-whitespace" ))
+  (#any-of? @Function.Builtin "\\" "\\s" "@read-next" "@parse-int" "@parse-int" "@read-until" "@parse-until" "@skip-whitespace" "obj>map" ))
 
 ((identifier) @Variable.Builtin
   (#any-of? @Variable.Builtin self))
